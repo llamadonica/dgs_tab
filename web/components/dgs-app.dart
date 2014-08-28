ebfe95637b035a80ac3b048bd17051a3898371be
Adam Stark <llamadonica@gmail.com>
2014-08-27 17:08:11 -0700

    .

--- web/components/dgs-app.dart
+++ web/components/dgs-app.dart
@@ -1,13 +1,43 @@
 import 'package:polymer/polymer.dart';
+import 'dart:html';
+import '../model/project.dart';
 
 /**
  * A Polymer click counter element.
  */
 @CustomTag('dgs-app')
 class DgsApp extends PolymerElement {
-  @observable String selected = "splash";
+  @observable String selected = 'splash';
   @observable final String responsiveWidth = '900px';
   @observable bool wide;
+  @observable Project project;
+  
+  static const Duration MIN_DURATION = const Duration(seconds: 1);
+  DateTime readyTime;
+  
   DgsApp.created() : super.created() {
+  }
+  
+  @override ready() {
+    readyTime = new DateTime.now();
+    if (window.history.state == null)
+      window.history.pushState({'app':'dgs'}, '');
+    window.onPopState.listen(popState);
+    if (project == null) {
+      var now = new DateTime.now();
+      asyncTimer(finishStartup,MIN_DURATION);
+    }
+  }
+  
+  void popState(PopStateEvent e) {
+    if (window.history.state == null)
+      window.history.pushState({'app':'dgs'}, '');
+  }
+  finishStartup() {
+    if (project == null) {
+      this.selected = 'pick-project';
+    } else {
+      this.selected = 'categories';
+    }
   }
 }
