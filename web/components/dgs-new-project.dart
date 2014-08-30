f2694aa1afe5121fc4a18698604e6e6fbaf43a08
Adam Stark <llamadonica@gmail.com>
2014-08-30 12:38:15 -0700

    .

--- web/components/dgs-new-project.dart
+++ web/components/dgs-new-project.dart
@@ -1,4 +1,5 @@
 import 'package:polymer/polymer.dart';
+import 'dart:html';
 import '../models.dart';
 
 /**
@@ -9,5 +10,19 @@ class DgsNewProject extends PolymerElement {
   @published bool wide;
   @published Project project;
   
+  static const String LAST_ELEMENT = 'name';
+  static List<String> fields = ['name'];
+  
   DgsNewProject.created() : super.created();
+  void handleLogin() {
+    
+  }
+  void keypressAction(KeyboardEvent ev) {
+    var keyStroke = (ev.which is int)?ev.which:ev.keyCode;
+    if (keyStroke == 13) {
+      if (ev.target == $[LAST_ELEMENT] )
+        ;
+    }
+  }
+  
 }
